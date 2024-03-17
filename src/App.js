6718202971a5876e0db69996a4de7e6586bed7f7
YashRuikar <yashruikar04@gmail.com>
2024-03-17 22:08:29 +0530

    Styled Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/GlobalStyles";
 
-function App() {
+const App = () => {
+  // const Button = styled.button``;
+
+  // const Para = styled.p``;
+
+  let myFavColor = "red";
+
+  const theme = {
+    color: {
+      textColor: "yellow",
+      bg: "red"
+    },
+    responsive: {
+      mobile: "768px",
+      tab: "998",
+    },
+  }
+
+  const Button = styled.button`
+    display: inline-block;
+    padding: 10px 20px;
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: none;
+    border: 2px solid #007bff;
+    color: #007bff;
+    // color: ${myFavColor};
+    background-color: ${({theme}) => theme.color.bg};
+    // background-color: ${({ bg }) => bg};
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s, color 0.3s;
+    // background: skyblue;
+
+    &:hover {
+      background-color: #007bff;
+      color: #fff;
+    }
+
+    &:focus {
+      outline: none;
+      box-shadow: 0 0 0 3px rgba(0, 123, 255, 0.3);
+    }
+
+    &:active {
+      background-color: #0056b3;
+      border-color: #0056b3;
+      color: #fff;
+    }
+
+    &:after {
+      content: "👍";
+    }
+
+    /*
+    span {
+      color: red;
+
+      // b{
+      //   color: #fff;
+      // }
+
+      .test {
+        color: #fff; // Same as above b tag
+      }
+
+      &:hover {
+        color: #fff;
+      }
+    }
+    */
+
+    // @media (max-width:${({theme}) => theme.responsive.mobile})
+  `;
+
+  const Para = styled.p`
+    color: grey;
+    font-size: 20px;
+  `;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        <Button className="btn" bg="yellow">
+          Styled Component
+        </Button>
+        <Para>Yash Ruikar</Para>
+      </>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizzing: border-box;
+}
+
+body{
+    height: 100vh;
+    display: grid;
+    place-items: center;
+    background-color: grey;
+  }
+  
+`;
